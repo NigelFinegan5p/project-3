ac9daf2788e74a11ad5ff791a5e5d7f7dc2b0c74
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2024-09-09 10:51:54 +0000

    import & configure products available and associated charges

--- run.py
+++ run.py
@@ -1,2 +1,21 @@
-# Your code goes here.
-# initial commit adding add to run.py
+'''Golf Clubs to Hire'''
+import re
+import sys
+
+'''Golf Clubs to Hire Ordering System'''
+#Configure ordering system #
+#The maximum number od sets of golf clubs per order
+MAX_CLUBS = 5
+#Delivery charge to Hotel & Apartment != ( price of delviery in € )
+DELIVERY_CHARGE = 15
+# List of club sets available and price
+
+CLUBS_AVAILABLE = {
+    "Titleist": 95,
+    "Taylor Made": 95,
+    "Srixon": 85,
+    "Callaway": 85,
+    "Srixon": 80,
+    "Wilson": 80
+
+}
